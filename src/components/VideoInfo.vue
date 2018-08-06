2368625931b0c74a82f0fb379ba131f4eaa43629
guo <guo565656@gmail.com>
2018-08-06 17:53:36 +0800

    no message

--- src/components/VideoInfo.vue
+++ src/components/VideoInfo.vue
@@ -1,10 +1,33 @@
 <template>
 <div class="VideoInfo">
+  <p>
+    <img src="http://img.d1w8.space:80/user_head/default_1_1.jpg">
+    <span>dsfss</span>
+  </p>
+  <p>
+    <span></span><span>发布</span>
+  </p>
+  <p>
+    <span>视频简介：</span><span>清纯嫩妹情趣套装无套内射 娇喘声听的心痒痒</span>
+  </p>
+  <p>
+    <ul>
+      <li>高身长</li>
+      <li>高身长</li>
+      <li>高身长</li>
+      <li>高身长</li>
+      <li>高身长</li>
+      <li>高身长</li>
+      <li>高身长</li>
+    </ul>
+  </p>
 </div>
 </template>
 <script>
 import Hub from '@/components/Hub';
-import {network} from '@/config/config';
+import {
+  network
+} from '@/config/config';
 export default {
   data() {
     return {}
@@ -24,12 +47,30 @@ export default {
 
 <style lang="scss" scoped>
 .VideoInfo {
-  width: 68%;
-  margin: 5px auto;
-  text-align: left;
-  float: left;
-  height: 200px;
-  background: #aab5b9;
-  padding: 5px;
+    width: 68%;
+    margin: 5px auto;
+    text-align: left;
+    float: left;
+    height: 200px;
+    background: #fff;
+    padding: 5px;
+    p {
+        font-size: 15px;
+        img {
+            margin-top: 20px;
+            height: 80px;
+            width: 80px;
+            border-radius: 50%;
+            vertical-align: middle;
+        }
+        span {
+            font-size: 15px;
+        }
+        ul {
+            li {
+                display: inline-block;
+            }
+        }
+    }
 }
 </style>
